e1658e23d2bb0f338952ed165cf26d91e37be7df
Gabyfle <gaby.santamaria@outlook.fr>
2019-03-30 20:27:46 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * gLoad-Screen 
+ * index.php
+ * 
+ * Handle requests and generate the right page by getting
+ * datas from the core app
+ * 
+ * @author Gabriel Santamaria <gaby.santamaria@outlook.fr>
+ * @version 1.0-beta
+ */
